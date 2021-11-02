b4641e56e8813617a8eb593aa1fb02e9432e31b6
nicolas <git config --local user.email your-email-address>
2021-11-02 15:04:33 -0500

    Creando app rftek

--- src/pages/Reportes.js
+++ src/pages/Reportes.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Chart from "react-google-charts";
- function Reportes() {
+function Reportes() {
     return (
         <div>
-              <div class="container" >
+            <div class="d-flex justify-content-center" >
                 <Chart
                     width={400}
                     height={300}
@@ -31,32 +31,34 @@ import Chart from "react-google-charts";
                     legendToggle
                 />
             </div>
-            <Chart
-  width={1000}
-  height={350}
-  chartType="Calendar"
-  loader={<div>Loading Chart</div>}
-  data={[
-    [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }],
-    [new Date(2012, 3, 13), 37032],
-    [new Date(2012, 3, 14), 38024],
-    [new Date(2012, 3, 15), 38024],
-    [new Date(2012, 3, 16), 38108],
-    [new Date(2012, 3, 17), 38229],
-    [new Date(2013, 1, 4), 38177],
-    [new Date(2013, 1, 5), 38705],
-    [new Date(2013, 1, 12), 38210],
-    [new Date(2013, 1, 13), 38029],
-    [new Date(2013, 1, 19), 38823],
-    [new Date(2013, 1, 23), 38345],
-    [new Date(2013, 1, 24), 38436],
-    [new Date(2013, 2, 10), 38447],
-  ]}
-  options={{
-    title: 'Red Sox Attendance',
-  }}
-  rootProps={{ 'data-testid': '1' }}
-/>
+            <div class="d-flex justify-content-center" >
+                <Chart
+                    width={1000}
+                    height={350}
+                    chartType="Calendar"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }],
+                        [new Date(2012, 3, 13), 37032],
+                        [new Date(2012, 3, 14), 38024],
+                        [new Date(2012, 3, 15), 38024],
+                        [new Date(2012, 3, 16), 38108],
+                        [new Date(2012, 3, 17), 38229],
+                        [new Date(2013, 1, 4), 38177],
+                        [new Date(2013, 1, 5), 38705],
+                        [new Date(2013, 1, 12), 38210],
+                        [new Date(2013, 1, 13), 38029],
+                        [new Date(2013, 1, 19), 38823],
+                        [new Date(2013, 1, 23), 38345],
+                        [new Date(2013, 1, 24), 38436],
+                        [new Date(2013, 2, 10), 38447],
+                    ]}
+                    options={{
+                        title: 'Red Sox Attendance',
+                    }}
+                    rootProps={{ 'data-testid': '1' }}
+                />
+            </div>
         </div>
     )
 }
